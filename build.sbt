c9b60687024a6942f97cb4608b27fc06e368bfd1
Josh Suereth <joshua.suereth@gmail.com>
2011-10-26 08:37:14 -0400

    Cel build now works up through specs

--- build.sbt
+++ build.sbt
@@ -1,5 +1,9 @@
 resolvers += ScalaToolsSnapshots
 
+organization := "org.scala-lang.cel"
+
+version := "cel-1.0-SNAPSHOT"
+
 scalaVersion := "2.10.0-SNAPSHOT"
 
 scalacOptions in Compile ++= Seq("-deprecation", "-unchecked")
--- project/Build.scala
+++ project/Build.scala
@@ -13,11 +13,10 @@ case class MyDependencyInfo(project: ProjectRef,
 case class MyDependencyActions(info: MyDependencyInfo,
                                addProjectDependency: Seq[ModuleID] = Nil,
                                removeLibraryDependency: Seq[ModuleID] = Nil)
+
+case class CelInfo(version: String, organization: String, scalaVersion: String)
                                
 trait DependencyAnalysis {
-  val celVersion = "cel-1.0-SNAPSHOT"
-  val celOrganization = "org.scala-lang.cel"
-  val celScalaVersion = "2.9.1"
   def hashInfo(d: MyDependencyInfo) = hashSynonym(d.organization + ":" + d.name)
   def hashModule(o: ModuleID) = hashSynonym(o.organization + ":" + o.name)
   def hashSynonym(x: String) = x match {
@@ -38,30 +37,56 @@ trait DependencyAnalysis {
     }
   
   /** Figures out which libraryDependencies on a project should be moved to project dependencies. */
-  def analyseDependencies(results: Vector[MyDependencyInfo]): Seq[MyDependencyActions] = {
+  def analyseDependencies(results: Vector[MyDependencyInfo], info: CelInfo): Seq[MyDependencyActions] = {
     val lookUp = (Map[String, MyDependencyInfo]() /: results) { (m, value) =>
        m + (hashInfo(value) -> value)
     }
-    
-    println("---- Dependencies = " + lookUp.mkString("\n\t", "\n\t", "\n"))
     (results map { value =>
       val changes = for { dep <- value.dependencies
         proj <- lookUp.get(hashModule(dep))
-      } yield (dep.copy(organization=celOrganization, revision=celVersion), dep)
-      // TODO - If we change the groupIds, we need to change the project moduleID group ids... maybe...
+      } yield (dep.copy(organization=info.organization, revision=info.version), dep)
       MyDependencyActions(value, changes map (_._1), changes map (_._2))
     } 
     filterNot (_.addProjectDependency.isEmpty) 
     filterNot (_.removeLibraryDependency.isEmpty))
   }
-  
+  /** Transforms a set of settings so that the CEL build will succeed. */
+  def fixCellProjectSettings(actions: Seq[MyDependencyActions], info: CelInfo)(settings: Seq[Setting[_]]) = {
+    def fixLibraryDependencies(s: Setting[_]): Setting[_] = if(s.key.scope.project.isInstanceOf[Select[_]]) s.asInstanceOf[Setting[Seq[ModuleID]]] mapInit { (_, old) =>        
+      val Scope(Select(ref), _, _, _) = s.key.scope
+      val toRemove = (for {
+        proj <- actions
+        if proj.info.project == ref
+        dep <- proj.removeLibraryDependency
+      } yield dep).toSet
+      val toAdd = (for {
+        proj <- actions
+        if proj.info.project == ref
+        dep <- proj.addProjectDependency
+      } yield dep)
+      (old filterNot toRemove) ++ toAdd
+      // TODO - Add new dependencies!
+    } else s
+    // Now fix dependencies
+    def f(s: Setting[_]): Setting[_] = s.key.key match {
+      // TODO - Create a *real* CEL version
+      case version.key             => s.asInstanceOf[Setting[String]].mapInit((_,_) => info.version)
+      case organization.key        => s.asInstanceOf[Setting[String]].mapInit( (_,_) => info.organization)
+      case scalaVersion.key        => s.asInstanceOf[Setting[String]].mapInit((_,_) => info.scalaVersion)
+      case resolvers.key           => s.asInstanceOf[Setting[Seq[Resolver]]].mapInit((_,old) => (old :+ ScalaToolsSnapshots).toSet.toSeq)
+      case libraryDependencies.key => fixLibraryDependencies(s)
+      case _ => s
+    } 
+    settings map f
+  }
 }
                                
 object CommunityExtensionsBuild extends Build with DependencyAnalysis {
-  
+  //val info = CelInfo("cel-1.0-SNAPSHOT", "org.scala-lang.cel", "2.9.1")
   lazy val celFixed = AttributeKey[Boolean]("scala-cel-references-fixed")
   lazy val root = Project("root", file(".")) dependsOn(projectDeps: _*) settings(
     commands += celSetup,
+    commands += celTest,
     onLoad in Global <<= (onLoad in Global) ?? idFun[State],
     onLoad in Global <<= (onLoad in Global) apply ( _ andThen ("cel-setup" :: _))
   )
@@ -78,37 +103,25 @@ object CommunityExtensionsBuild extends Build with DependencyAnalysis {
   lazy val projectRefs: Seq[ProjectReference] = Seq(scalaArm, scalaCheck, scalaz, specs2)
   lazy val projectDeps: Seq[ClasspathDependency] = projectRefs map (new ClasspathDependency(_, None))
 
-  /** Transforms a set of settings so that the CEL build will succeed. */
-  def fixCellProjectSettings(actions: Seq[MyDependencyActions])(settings: Seq[Setting[_]]) = {
-    def fixLibraryDependencies(s: Setting[_]): Setting[_] = if(s.key.scope.project.isInstanceOf[Select[_]]) s.asInstanceOf[Setting[Seq[ModuleID]]] mapInit { (_, old) =>        
-      val Scope(Select(ref), _, _, _) = s.key.scope
-      val toRemove = (for {
-        proj <- actions
-        if proj.info.project == ref
-        dep <- proj.removeLibraryDependency
-      } yield dep).toSet
-      val toAdd = (for {
-        proj <- actions
-        if proj.info.project == ref
-        dep <- proj.addProjectDependency
-      } yield dep)
-      println("Project ["+ref+"] toAdd = " + toAdd.mkString("\n\t", "\n\t", "\n"))
-      (old filterNot toRemove) ++ toAdd
-      // TODO - Add new dependencies!
-    } else s
-    // Now fix dependencies
-    def f(s: Setting[_]): Setting[_] = s.key.key match {
-      // TODO - Create a *real* CEL version
-      case version.key             => s.asInstanceOf[Setting[String]].mapInit((_,_) => celVersion)
-      case organization.key        => s.asInstanceOf[Setting[String]].mapInit( (_,_) => celOrganization)
-      case scalaVersion.key        => s.asInstanceOf[Setting[String]].mapInit((_,_) => celScalaVersion)
-      case resolvers.key           => s.asInstanceOf[Setting[Seq[Resolver]]].mapInit((_,old) => (old :+ ScalaToolsSnapshots).toSet.toSeq)
-      case libraryDependencies.key => fixLibraryDependencies(s)
-      case _ => s
-    } 
-    settings map f
+  def celTest = Command.command("cel-test") { (state: State) =>
+    val extracted = Project.extract(state)
+    import extracted._ 
+    val magikTasks = Seq(test in Test, publishLocal)
+    for(ref <- projectRefs; task <- magikTasks) {
+      // TODO - Better print statements
+      println("Executing " + task.key + " in " + ref)
+      Project.evaluateTask(task in ref, state)
+    }
+    state
   }
-  
+ 
+  /** Pulls the name/organization/version for each project in the CEL build */
+  def getCelInfo(extracted: Extracted): CelInfo =
+    CelInfo(extracted.get(Keys.version in root),
+            extracted.get(Keys.organization in root),
+            extracted.get(Keys.scalaVersion in root))
+
+ 
   // Define the command.  This takes the existing settings (including any session settings)
   // and applies 'f' to each Setting[_]
   def celSetup = Command.command("cel-setup") { (state: State) =>
@@ -118,14 +131,13 @@ object CommunityExtensionsBuild extends Build with DependencyAnalysis {
       val extracted = Project.extract(state)
       import extracted._ 
       // Bump Scala version and build projects.
+      val info = getCelInfo(extracted)
       val refs = (session.mergeSettings map (_.key.scope) collect {
         case Scope(Select(p @ ProjectRef(_,_)),_,_,_) => p
       } toSet)
       val projectInfos = getProjectInfos(extracted, refs)
-      val projectActions = analyseDependencies(projectInfos)
-      println("--== Project actions ==--")
-      projectActions foreach println
-      val transformedSettings = fixCellProjectSettings(projectActions)(session.mergeSettings)
+      val projectActions = analyseDependencies(projectInfos, info)
+      val transformedSettings = fixCellProjectSettings(projectActions, info)(session.mergeSettings)
       // Now we need to rip into structure and add references to appropriate projects.
       import Load._      
       val newStructure2 = Load.reapply(transformedSettings, structure)
